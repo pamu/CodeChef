b1dacc2a06b85d7035c8a8c1e39b34821d8902c4
pamu <nagarjuna.pamu@gmail.com>
2015-03-15 19:18:02 +0530

    test

--- src/main/scala/com/codechef/problems/test/Main.scala
+++ src/main/scala/com/codechef/problems/test/Main.scala
@@ -1,15 +1,17 @@
 package com.codechef.problems.test
 
-object Main extends App {
-  import java.io._
-  val reader = new BufferedReader(new InputStreamReader(System.in))
-  try{
-    while(true) {
-      val c = reader.readLine.toInt
-      if(c == 42) throw new Exception("stop") else println(c)
+object Main {
+  def main(args: Array[String]): Unit = {
+    import java.io._
+    val reader = new BufferedReader(new InputStreamReader(System.in))
+    try{
+      while(true) {
+        val c = reader.readLine.toInt
+        if(c == 42) throw new Exception("stop") else println(c)
+      }
     }
-  }
-  catch {
-    case ex: Exception =>  
+    catch {
+      case ex: Exception =>  
+    } 
   }
 }
