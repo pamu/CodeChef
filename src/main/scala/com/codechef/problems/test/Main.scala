554df025bdff10befa9ead23f2b2e913b13a14f5
pamu <nagarjuna.pamu@gmail.com>
2015-03-15 19:15:12 +0530

    test

--- src/main/scala/com/codechef/problems/test/Main.scala
+++ src/main/scala/com/codechef/problems/test/Main.scala
@@ -1,5 +1,15 @@
 package com.codechef.problems.test
 
 object Main extends App {
-  
+  import java.io._
+  val reader = new BufferedReader(new InputStreamReader(System.in))
+  try{
+    while(true) {
+      val c = reader.readLine.toInt
+      if(c == 42) throw new Exception("stop") else println(c)
+    }
+  }
+  catch {
+    case ex: Exception =>  
+  }
 }
